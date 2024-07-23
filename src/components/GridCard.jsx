3bf9cb972276073cfc62a5adb4ce9077ee79c3c0
Shingyu Kang <rkdtlseb@naver.com>
2024-07-23 22:02:44 +0900

    apply prop to GridCard, extract array data to a seperate file, use onMouseOver event

--- src/components/GridCard.jsx
+++ src/components/GridCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../styles/gridCard.css";
-export const GridCard = () => {
+export const GridCard = ({ content, onMouseOver }) => {
   //Component 를 인자로 받아서 hover 되면 Component 를 보여주는 코드
   return (
     <>
-      <div className="gridCard">
+      <div className="gridCard" onMouseOver={onMouseOver}>
         <div className="gridImage">GridCard</div>
         <div className="gridTitle">Title</div>
       </div>
--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 import "../styles/gridContainer.css";
 import { GridCard } from "./GridCard";
+import gridItems from "../assets/gridItems";
 
+const handleMouseOver = () => {
+  console.log("remove same row = add css hidden");
+};
 export const GridContainer = () => {
   return (
     <div className="gridContainer">
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
-      <GridCard />
+      {gridItems.map((item, index) => (
+        <GridCard key={item.id} content={item.content} onMouseOver={() => handleMouseOver()} />
+      ))}
     </div>
   );
 };
