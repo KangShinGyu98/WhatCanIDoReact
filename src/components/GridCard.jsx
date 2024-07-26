0b4e73524f83249f2cbcbe678d83d5f0dbb4f442
Shingyu Kang <rkdtlseb@naver.com>
2024-07-26 20:59:42 +0900

    change hover => click

--- src/components/GridCard.jsx
+++ src/components/GridCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../styles/gridCard.css";
-export const GridCard = ({ content, onMouseOver }) => {
+export const GridCard = ({ content, hanleClick }) => {
   //Component 를 인자로 받아서 hover 되면 Component 를 보여주는 코드
   return (
     <>
-      <div className="gridCard" onMouseOver={onMouseOver}>
+      <div className="gridCard" onClick={hanleClick}>
         <div className="gridImage">GridCard</div>
         <div className="gridTitle">Title</div>
       </div>
--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -3,14 +3,14 @@ import "../styles/gridContainer.css";
 import { GridCard } from "./GridCard";
 import gridItems from "../assets/gridItems";
 
-const handleMouseOver = () => {
+const hanleClick = () => {
   console.log("remove same row = add css hidden");
 };
 export const GridContainer = () => {
   return (
     <div className="gridContainer">
       {gridItems.map((item, index) => (
-        <GridCard key={item.id} content={item.content} onMouseOver={() => handleMouseOver()} />
+        <GridCard key={item.id} content={item.content} hanleClick={() => hanleClick()} />
       ))}
     </div>
   );
