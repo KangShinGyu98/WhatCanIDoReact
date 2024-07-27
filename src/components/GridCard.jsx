940a6ccf617f6b4989496f31f8cb07c1f6a3b324
Shingyu Kang <rkdtlseb@naver.com>
2024-07-27 17:33:45 +0900

    scroll view

--- src/components/GridCard.jsx
+++ src/components/GridCard.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../styles/gridCard.css";
 
 export const GridCard = ({ containerRef, content }) => {
   //Component 를 인자로 받아서 hover 되면 Component 를 보여주는 코드
+  const cardRef = useRef(null);
   const [isClicked, setIsClicked] = useState(false);
   const hanleClick = () => {
     console.log(containerRef.current.offsetWidth);
     setIsClicked(!isClicked);
   };
+  useEffect(() => {
+    if (isClicked && cardRef.current) {
+      cardRef.current.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  }, [isClicked]);
+
   const gridCardStyle = {
     backgroundColor: isClicked ? "yellow" : "chartreuse",
     height: isClicked ? Math.max(containerRef.current.offsetWidth * 0.5625, 320) : "100%",
     gridColumn: isClicked ? "1 / -1" : "",
-    borderRadius: "5%",
+    borderRadius: "30px",
     display: "flex",
     flexDirection: "column",
     width: "100%",
@@ -23,12 +30,12 @@ export const GridCard = ({ containerRef, content }) => {
 
   const gridCardImageStyle = {
     background: `white url("/assets/griditem.png") no-repeat left top`,
-    borderTop: "5%",
-    height: "280px",
+    height: "90%",
+    // height: "280px",
   };
   return (
     <>
-      <div className="gridCard" onClick={() => hanleClick()} style={gridCardStyle}>
+      <div ref={cardRef} className="gridCard" onClick={() => hanleClick()} style={gridCardStyle}>
         <div className="gridImage" style={gridCardImageStyle}>
           GridCard
         </div>
