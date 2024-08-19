e4a9136cdb3cfb524ffd687b6b53ef2e8df04cc2
Shingyu Kang <rkdtlseb@naver.com>
2024-08-19 21:01:34 +0900

    [배포]

---
+++ build/assets/gridItems.jsx
@@ -0,0 +1,12 @@
+const gridItems = [
+  { id: 0, content: <div>item1</div> },
+  { id: 1, content: <div>item2</div> },
+  { id: 2, content: <div>item3</div> },
+  { id: 3, content: <div>item4</div> },
+  { id: 4, content: <div>item5</div> },
+  { id: 5, content: <div>item6</div> },
+  { id: 6, content: <div>item7</div> },
+  { id: 7, content: <div>item8</div> },
+];
+
+export default gridItems;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // 빌드 결과물이 저장될 디렉토리 이름을 'build'로 설정
+  },
+});
