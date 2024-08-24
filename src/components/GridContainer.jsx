c0ebcb5989e826ef728b5d404b1a4921a2efbb41
Shingyu Kang <rkdtlseb@naver.com>
2024-08-24 22:19:20 +0900

    url change

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -2,15 +2,10 @@ import React, { useRef, useState } from "react";
 import "../styles/gridContainer.css";
 import { GridCard } from "./GridCard";
 import { NeubrutalButton } from "./InnerComponents/NeubrutalButton";
+import { NavBar } from "./InnerComponents/NavBar";
 const gridItems = [
-  { id: 0, content: <NeubrutalButton />, imagePath: "/assets/neubrutalButton.png" },
-  { id: 1, content: <div>item2</div>, imagePath: "/assets/griditem.png" },
-  { id: 2, content: <div>item3</div>, imagePath: "/assets/griditem.png" },
-  { id: 3, content: <div>item4</div>, imagePath: "/assets/griditem.png" },
-  { id: 4, content: <div>item5</div>, imagePath: "/assets/griditem.png" },
-  { id: 5, content: <div>item6</div>, imagePath: "/assets/griditem.png" },
-  { id: 6, content: <div>item7</div>, imagePath: "/assets/griditem.png" },
-  { id: 7, content: <div>item8</div>, imagePath: "/assets/griditem.png" },
+  { id: 0, content: <NeubrutalButton />, imagePath: "./assets/neubrutalButton.png" },
+  { id: 1, content: <NavBar />, imagePath: "./assets/griditem.png" },
 ];
 
 export const GridContainer = () => {
